985e07c6b8767c6014db432dc90f747cdbc3b131
Lovish Singla <lovish.singla8283@gmail.com>
2022-11-03 12:27:20 +0530

    day3 completed

--- sampleproject/lib/main.dart
+++ sampleproject/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sampleproject/homepage.dart';
+import 'package:sampleproject/pages/homepage.dart';
+import 'package:sampleproject/pages/loginpage.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,8 +9,17 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // widget is like we use int double string it return a widget
     return MaterialApp(
-      home: Homepage(),
+      
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/":(context) => loginpage()
+      },
     );
   }
 }
